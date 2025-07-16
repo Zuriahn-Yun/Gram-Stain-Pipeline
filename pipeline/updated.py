05f50546085d08b16135b690d7ffdfbd1a7eea16
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-07-16 10:55:52 -0700

    Updated.py is a different approach
    The CSS webcolors does not have a color for everything and you would need to roudn to get results

--- pipeline/updated.py
+++ pipeline/updated.py
@@ -7,17 +7,49 @@ import matplotlib.pyplot as plt
 import plotly
 import webcolors 
 
+def darkness_luminosity(row):
+  """ Using the NTSC formula to convert RGB values into Grayscale, this formula closely represents the average persons relative perception of brighness in red,green and blue light. Smaller values represent darker pixels.
+
+  Args:
+      row (int): a row from an image dataframe and contains red,green and blue columns
+  Returns:
+      int : darkness value
+  """
+  return (0.299 * row['Red'] + 0.587 * row['Green'] + 0.114 * row['Blue'])
+
+def get_color(row):
+    """ Turn RGB from each row to string name """
+    red = row['Red']
+    green = row['Green']
+    blue = row['Blue']
+    rgb = (red,green,blue)  
+    
+    try:
+        color = webcolors.rgb_to_name(rgb)  
+    except ValueError as e:
+        
+        color = "Has no defined color name"
+    return color
+
 def get_rgb(file_path):
     
     """ Convert Image to RGB -> DataFrame """
     image = Image.open(file_path)
     image_array = np.array(image)
-    image_array.reshape(-1,3)
-    df = pd.DataFrame(image_array)
+    
+    """ Flatten the Array, new size is (1440 * 1920, 3) """
+    pixels = image_array.reshape(-1,3)
+    df = pd.DataFrame(pixels)
     
     """ Name Columns """
-    df.Columns =["Red","Green","Blue"]
-    return df
+    df.columns =["Red","Green","Blue"]
+    
+    df['Color'] = df.apply(get_color,axis=1)
+    return df    
 
-output = get_rgb("pipeline/test.jpg")
-print(output)
+# output = get_rgb("pipeline/test.jpg")
+# print(output)
+
+rgb = (38,19,41)
+print(webcolors.rgb_to_name(rgb))
+
